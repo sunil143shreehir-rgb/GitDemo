d52e836500133c1ccc60c70044a47a885a1b4cce
sunil143shreehir-rgb <sunil143shreehir@gmail.com>
2026-06-05 21:32:14 +0530

    adding playwright sample test case with Python

--- tests/reusable.py
+++ tests/reusable.py
@@ -2,36 +2,44 @@ import re
 from playwright.sync_api import sync_playwright
 import playwright
 playwright = sync_playwright().start()
+
 firefox = playwright.firefox
 def call_to_ffbrowser():
     browser = firefox.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context(ignore_https_errors=True)
+    page = context.new_page()
     page.goto("https://www.google.com")
     page.wait_for_timeout(5000) 
     print(page.title())
     def test_google_title():
         return re.match(r"Google", page.title()) 
     assert test_google_title() is not None, "Title does not match 'Google'"
-    browser.close()
+    return playwright, browser, page,context
+
 cromium = playwright.chromium
 def call_to_chromium():
     browser = cromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context(ignore_https_errors=True)
+    page = context.new_page()
     page.goto("https://www.google.com")
     page.wait_for_timeout(5000) 
     print(page.title())
     def test_google_title():
         return re.match(r"Google", page.title()) 
     assert test_google_title() is not None, "Title does not match 'Google'"
-    browser.close()
+    return playwright, browser, page,context
+
 mecrosoft_edge = playwright.chromium.launch(channel="msedge", headless=False)
 def call_to_microsoft_edge():
+    browser = cromium.launch(headless=False)
+    context = browser.new_context(ignore_https_errors=True)
+    page = context.new_page()
     page = mecrosoft_edge.new_page()
     page.goto("https://www.google.com")
     page.wait_for_timeout(5000) 
     print(page.title())
     def test_google_title():
         return re.match(r"Google", page.title()) 
-    assert test_google_title() is not None, "Title does not match 'Google'"
-    mecrosoft_edge.close()  
+    assert test_google_title() is not None, "Title does not match 'Google'" 
+    return playwright, browser, page,context
 
--- tests/test1.py
+++ tests/test1.py
@@ -2,7 +2,13 @@ import re
 from playwright.sync_api import sync_playwright
 import playwright
 import reusable
-reusable.call_to_ffbrowser();
-print("Firefox test completed.")
-reusable.call_to_microsoft_edge();
+playwright, browser, page, context = reusable.call_to_microsoft_edge(); 
+page.goto("https://www.indianrail.gov.in/")
+page.wait_for_timeout(5000)
+print(page.title())
+if page.title() == "indianrailways.com - Passenger Enquiry":
+    print("Test passed: Title matches 'indianrailways.com - Passenger Enquiry'")
+else:
+    print("Test failed: Title does not match 'indianrailways.com - Passenger Enquiry'")  
 print("Microsoft Edge test completed.")
+browser.close()
