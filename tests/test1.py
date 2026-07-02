c85855031e251e040da945611c0a3a9e652b5839
sunil143shreehir-rgb <sunil143shreehir@gmail.com>
2026-07-02 16:28:52 +0530

    all

--- tests/test1.py
+++ tests/test1.py
@@ -1,4 +1,5 @@
 import re
+#import requests
 from playwright.sync_api import sync_playwright
 import playwright
 import reusable
@@ -6,9 +7,29 @@ playwright, browser, page, context = reusable.call_to_microsoft_edge();
 page.goto("https://www.indianrail.gov.in/")
 page.wait_for_timeout(5000)
 print(page.title())
-if page.title() == "indianrailways.com - Passenger Enquiry":
-    print("Test passed: Title matches 'indianrailways.com - Passenger Enquiry'")
+if page.title() == "Welcome to Indian Railway Passenger Reservation Enquiry":
+    print("Test passed: Title matches - 'indian railways inquiry site opened successfully'")
 else:
-    print("Test failed: Title does not match 'indianrailways.com - Passenger Enquiry'")  
-print("Microsoft Edge test completed.")
+    print("Test failed: Title does not match - 'indian railways inquiry site does not opened successfully'")  
+##print("Microsoft Edge test completed.")
+
+reusable.call_to_api()
+print("API call completed successfully.")
+
+
+"""
+payload = {
+    "from": "BBS",
+    "to": "NDLS",
+    "date": "2026-07-02"
+}
+
+headers = {
+    "Authorization": "Bearer YOUR_TOKEN"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.json())
 browser.close()
+"""
